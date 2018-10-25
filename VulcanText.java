df838572870541e6abccfb689fe877cc1d22c3a7
Squigernog <44275774+Squigernog@users.noreply.github.com>
2018-10-25 02:04:36 -0700

    Update VulcanText.java

--- VulcanText.java
+++ VulcanText.java
@@ -10,7 +10,9 @@ import java.util.Scanner;
 
 /**
  *
- * @author kermi
+ * @author Omar and Luke
+ *output: messages translated into alien languages from earth language
+ *purpose: translate earth text messages into alien languages and sent them to alien cellphones
  */
 public class VulcanText implements AlienCellPhone
 {
