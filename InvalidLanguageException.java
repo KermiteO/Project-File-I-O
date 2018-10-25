b29cb9b17472a40a153482df27c3ac135f657af6
Squigernog <44275774+Squigernog@users.noreply.github.com>
2018-10-25 02:02:06 -0700

    Update InvalidLanguageException.java

--- InvalidLanguageException.java
+++ InvalidLanguageException.java
@@ -7,7 +7,9 @@ package lab4;
 
 /**
  *
- * @author kermi
+ * @author Omar and Luke
+ *output: messages translated into alien languages from earth language
+ *purpose: translate earth text messages into alien languages and sent them to alien cellphones
  */
 public class InvalidLanguageException extends Exception {
     public InvalidLanguageException(String e) {super(e);}
